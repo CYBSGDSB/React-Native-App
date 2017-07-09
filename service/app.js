06ba4426b5bd622cf5522b80e4db306b836c8a59
CYBSGDSB <ccyb55@outlook.com>
2017-07-09 10:50:21 +0800

    j接口的实现

--- service/app.js
+++ service/app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var data = require('./routes/data');
 
 var app = express();
 
@@ -24,16 +26,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/data', data);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+
+app.use(session({
+  secret: '#sddjswjdhww22ygfw2233@@@%#$!%*12',
+  resave: false,
+  saveUninitialized: true
+}));
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
